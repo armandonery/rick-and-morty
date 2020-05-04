12ac421bc922d4e96f09c8a1324b61e33aca0689
Armando Nery <wespenry@hotmail.com>
2020-05-04 15:01:24 -0500

    Updates

---
+++ precache-manifest.dcee461a51455c4b88ca98954848a4b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e760d793be6039c80256b44e21ec4eb",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "2ebe613cf390d0fa2b6d",
+    "url": "/rick-and-morty/static/css/main.d7720e8a.chunk.css"
+  },
+  {
+    "revision": "53e1a0e67d2eb7a968db",
+    "url": "/rick-and-morty/static/js/2.b3cdcf92.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/rick-and-morty/static/js/2.b3cdcf92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebe613cf390d0fa2b6d",
+    "url": "/rick-and-morty/static/js/main.0269b0c4.chunk.js"
+  },
+  {
+    "revision": "79933579ddc487046afc",
+    "url": "/rick-and-morty/static/js/runtime-main.e26ca37b.js"
+  },
+  {
+    "revision": "b1b631b0581669ffd403c7b9ada659a9",
+    "url": "/rick-and-morty/static/media/css.b1b631b0.svg"
+  },
+  {
+    "revision": "7562dd8613ccd14e88b015682130eed3",
+    "url": "/rick-and-morty/static/media/logo.7562dd86.svg"
+  },
+  {
+    "revision": "ba99aecd075dddfb01b64b37724165fb",
+    "url": "/rick-and-morty/static/media/material-ui.ba99aecd.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/rick-and-morty/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "59075df5c2c4f1cd760034431a7e60bf",
+    "url": "/rick-and-morty/static/media/wall2.59075df5.jpg"
+  }
+]);
